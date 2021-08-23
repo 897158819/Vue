682f0357075cf5421207cd3d50df607e6e4382a3
unknown <897158819@qq.com>
2021-08-23 23:34:30 +0800

    add element-ui

---
+++ src/element/elementIndex.js
@@ -0,0 +1,8 @@
+import { ElButton } from "element-plus";
+
+const element = {
+    install:function(Vue) {
+        Vue.use(ElButton)
+    }
+}
+export default element
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './store/store'
+import element from './element/elementIndex'
+import './style/base.css'
+import 'element-plus/lib/theme-chalk/index.css'
+createApp(App).use(store).use(router).use(element).mount('#app') 
 
-createApp(App).use(store).use(router).mount('#app')
+
+// const app = createApp(App)
+// app.use(
+//     store,
+//     router,
+//     element
+// ).mount('#app')
