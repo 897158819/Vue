8255de3f385f00995c23f6fab634601c52f6541c
Fuwen yeung <yfw897158819@outlook.com>
2021-08-27 17:27:18 +0800

    update find index page

--- src/component/Find/recommend.vue
+++ src/component/Find/recommend.vue
@@ -16,17 +16,21 @@
         <div class="recommend_content">
           <div class="recommend_content_left">
             <div class="recommend_hot">
-              <div class="hot_nav">
-                <div class="hot_nav_title">热门推荐</div>
-                <div class="hot_nav_option"><a href="">华语</a></div>
-                <div class="hot_nav_option"><a href="">流行</a></div>
-                <div class="hot_nav_option"><a href="">民谣</a></div>
-                <div class="hot_nav_option"><a href="">摇滚</a></div>
-                <div class="hot_nav_option"><a href="">电子</a></div>
-                <div class="hot_nav_more"><a href="">更多</a></div>
+              <div class="recommend_hot_nav">
+                <div class="recommend_hot_nav_title">热门推荐</div>
+                <div class="recommend_hot_nav_option"><a href="">华语</a></div>
+                <div class="recommend_hot_nav_option"><a href="">流行</a></div>
+                <div class="recommend_hot_nav_option"><a href="">民谣</a></div>
+                <div class="recommend_hot_nav_option"><a href="">摇滚</a></div>
+                <div class="recommend_hot_nav_option"><a href="">电子</a></div>
+                <div class="recommend_hot_nav_option"><a href="">更多</a></div>
               </div>
-              <div class="hot_content">
+              <div class="recommend_hot_content">
                 <ul>
+                  <li>nihao</li>
+                  <li>nihao</li>
+                  <li>nihao</li>
+                  <li>nihao</li>
                   <li>nihao</li>
                   <li>nihao</li>
                   <li>nihao</li>
@@ -34,8 +38,43 @@
                 </ul>
               </div>
             </div>
+            <div class="recommend_individuality">
+              <div class="recommend_individuality_nav">
+                <div class="recommend_individuality_title">个性化推荐</div>
+              </div>
+              <div class="recommend_individuality_content">
+                  <ul>
+                    <li>nihao</li>
+                    <li>nihao</li>
+                    <li>nihao</li>
+                    <li>nihao</li>
+                  </ul>
+                </div>
+            </div>
+            <div class="recommend_disc">
+              <div class="recommend_disc_nav">
+                <div class="recommend_disc_title">新碟上架</div>
+              </div>
+              <div class="recommend_disc_content">
+
+              </div>
+            </div>
+            <div class="recommend_list">
+              <div class="recommend_list_nav">
+                <div class="recommend_list_title">榜单</div>
+              </div>
+              <div class="recommend_list_content">
+
+              </div>
+            </div>
           </div>
           <div class="recommend_content_right">
+            <div class="recommend_personInfo">
+              <div class="recommend_personInfo_image">
+                <img src="" alt="">
+              </div>
+              
+            </div>
           </div>
         </div>
       </div>
